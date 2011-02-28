80da2d39791ff1090c325bf48e3192c88b92a5e5
andrzejsliwa <andrzej.sliwa@i-tool.eu>
2011-02-28 13:40:53 +0100

    Transformers! CouchApp transformation..  :)

---
+++ config.js
@@ -0,0 +1,19 @@
+{
+  "id": "tweeti",
+  "mapDirectories": {
+    "config.js": "",
+    "favicon.ico": "_attachments/favicon.ico",
+    "index.html": "_attachments/index.html",
+    "spec.html": "_attachments/spec.html",
+    "css": "_attachments/css",
+    "images": "_attachments/images",
+    "sass": false,
+    "js": "_attachments/js",
+    "spec": "_attachments/spec",
+    "templates": "_attachments/templates",
+    "db": "",
+    "Jimfile": false,
+    "hooks": false
+  }
+}
+
